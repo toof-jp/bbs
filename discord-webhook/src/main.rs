8bbcfad72eeddf721d835e00c364815bc00be876
Fumiya Tokumasu <toof@toof.jp>
2025-09-26 23:13:23 +0900

    feat: redis

--- discord-webhook/src/main.rs
+++ discord-webhook/src/main.rs
@@ -1,6 +1,4 @@
 use std::collections::HashMap;
-use std::fs::read_to_string;
-use std::path::Path;
 use std::thread::sleep;
 use std::time::Duration;
 use std::{env, fmt};
@@ -8,6 +6,7 @@ use std::{env, fmt};
 use anyhow::Result;
 use chrono::NaiveDateTime;
 use dotenvy::dotenv;
+use redis::AsyncCommands;
 use serde::Serialize;
 use sqlx::postgres::PgPoolOptions;
 use sqlx::PgPool;
@@ -19,20 +18,19 @@ async fn main() -> Result<()> {
     let webhook_url = env::var("WEBHOOK_URL").expect("WEBHOOK_URL must be set");
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     let image_url_prefix = env::var("IMAGE_URL_PREFIX").expect("IMAGE_URL_PREFIX must be set");
+    let redis_url = env::var("REDIS_URL").expect("REDIS_URL must be set");
 
     let pool = PgPoolOptions::new()
         .connect(&database_url)
         .await
         .expect("Failed to connect to Postgres");
 
-    let mut max_no = if Path::new("res_no").exists() {
-        read_to_string("res_no")
-            .unwrap()
-            .trim()
-            .parse::<i32>()
-            .unwrap()
-    } else {
-        get_max_res_no(&pool).await?
+    let redis_client = redis::Client::open(redis_url)?;
+    let mut redis_conn = redis_client.get_multiplexed_async_connection().await?;
+
+    let mut max_no = match redis_conn.get("max_no").await? {
+        Some(max_no) => max_no,
+        None => get_max_res_no(&pool).await?,
     };
 
     loop {
@@ -42,7 +40,7 @@ async fn main() -> Result<()> {
             post(&webhook_url, &res, &image_url_prefix).await?;
             eprintln!("posted: {}", res.no);
             max_no = res.no;
-            std::fs::write("res_no", format!("{}\n", max_no)).unwrap();
+            redis_conn.set("max_no", max_no).await?;
             sleep(Duration::from_millis(500));
         }
 
@@ -69,9 +67,10 @@ async fn post(webhook_url: &str, res: &Res, image_url_prefix: &str) -> Result<()
         content: res.to_string(),
         embeds: res.oekaki_id.map(|oekaki_id| {
             vec![DiscordEmbed {
-                image: HashMap::from([
-                    ("url".to_string(), format!("{}{}.png", image_url_prefix, oekaki_id))
-                ]),
+                image: HashMap::from([(
+                    "url".to_string(),
+                    format!("{}{}.png", image_url_prefix, oekaki_id),
+                )]),
             }]
         }),
     };
